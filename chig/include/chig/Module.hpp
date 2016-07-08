609ac754d0292053682fc1321356b2684dfc793d
Russell Greene <russellgreene8@gmail.com>
2016-07-08 10:20:01 -0600

    Add key to apt

--- chig/include/chig/Module.hpp
+++ chig/include/chig/Module.hpp
@@ -16,7 +16,8 @@ struct Module {
 	
 	virtual ~Module();
 	
-	virtual std::vector<std::function<NodeType(nlohmann::json& data)>> getNodeTypes() = 0;
+	virtual std::vector<std::function<std::unique_ptr<NodeType>(nlohmann::json& data)>> getNodeTypes() = 0;
+	virtual std::unique_ptr<NodeType> createNodeType(const char* name, const nlohmann::json& json_data) = 0;
 	
 	std::string name;
 	
