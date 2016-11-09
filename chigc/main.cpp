47af9e0f0a48fd25389b7c9799a4db5e4794b107
Russell Greene <russellgreene8@gmail.com>
2016-11-08 17:05:56 -0700

    Add stuff to things

--- chigc/main.cpp
+++ chigc/main.cpp
@@ -1,7 +1,11 @@
 #include <chig/Context.hpp>
 #include <chig/NodeType.hpp>
+#include <chig/LangModule.hpp>
+#include <chig/GraphFunction.hpp>
 
 #include <boost/program_options.hpp>
+
+#include <llvm/Support/raw_os_ostream.h>
 
 #include <iostream>
 #include <vector>
@@ -11,6 +15,63 @@ using namespace chig;
 
 int main(int argc, char** argv) {
 	
+	Context c;
+	c.addModule(std::make_unique<LangModule>(c));
+
+	std::unique_ptr<GraphFunction> func;
+	
+	auto jsondata = R"ENDJSON(
+		{
+				  "module": {
+						      "name": "main",
+							      "dependencies": [
+								        "io"
+										    ]
+											  },
+											    "graphs": [
+												    {
+															      "type": "function", 
+																        "name": "main",
+																		      "nodes": [
+																			          {
+																							            "type": "lang:entry",
+																										          "location": [0, 0],
+																												            "data": {
+																																	            "input": "lang:i32"
+																																				          }
+																																						          },
+																																								          {
+																																												            "type": "lang:exit",
+																																															          "location": [10, 0],
+																																																	            "data": {
+																																																						            "output": "lang:i32"
+																																																									          }
+																																																											          }
+																																																													        ],
+																																																															      "connections": [
+																																																																          {
+																																																																				            "type": "exec",
+																																																																							          "input": [0,0],
+																																																																									            "output": [1,0]
+																																																																												        },
+																																																																														        {
+																																																																																		          "type": "data",
+																																																																																				            "input": [0,0],
+																																																																																							          "output": [1,0]
+																																																																																									          }
+																																																																																											        ]
+																																																																																													    }
+																																																																																														  ]
+		}
+	)ENDJSON"_json;
+
+	Result r = GraphFunction::fromJSON(c, jsondata["graphs"][0], &func);
+	std::unique_ptr<llvm::Module> mod = std::make_unique<llvm::Module>("hello", c.context);
+	auto llfunc = func->compile(mod.get());
+
+	llvm::raw_os_ostream stream(std::cout);
+	mod->print(stream, nullptr);
+
 	namespace po = boost::program_options;
 	
 	po::options_description desc("chigc: Chigraph compiler command line parameters", 50);
