77f12bffc1836376f49636a9d06ded4cbedbd14e
Russell Greene <russellgreene8@gmail.com>
2016-11-10 14:16:58 -0700

    Work on jsonmodule

---
+++ chig/src/jsonmodule.cpp
@@ -0,0 +1,7 @@
+#include "chig/JsonModule.hpp"
+
+using namespace chig;
+
+JsonModule::JsonModule(const nlohmann::json& json_data, Context& cont, Result* r) : ChigModule(cont) {
+    // read stuff from module
+}
