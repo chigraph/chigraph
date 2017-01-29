7b449127aaf5a28ba003765945d7eab95effe074
Russell Greene <russellgreene8@gmail.com>
2017-01-29 09:13:59 -0700

    Work on travsi

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,14 +5,10 @@ set -xe
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 
 	sudo apt-get update
-	sudo apt-get install qt${QT_VERSION:0:2}base qt${QT_VERSION:0:2}script \
-		qt${QT_VERSION:0:2}declarative qt${QT_VERSION:0:2}tools \
-		qt${QT_VERSION:0:2}x11extras qt${QT_VERSION:0:2}svg  ninja-build \
-		libedit-dev libxcb-keysyms1-dev libxml2-utils gcc-5 gcc-6 g++-5 g++-6 \
-		build-essential clang-3.7 clang-3.8 clang-3.9 llvm-${LLVM_VERSION}-dev python-pip
+	sudo apt-get install qt${QT_VERSION:0:2}base qt${QT_VERSION:0:2}script qt${QT_VERSION:0:2}declarative qt${QT_VERSION:0:2}tools qt${QT_VERSION:0:2}x11extras qt${QT_VERSION:0:2}svg  ninja-build libedit-dev libxcb-keysyms1-dev libxml2-utils gcc-5 gcc-6 g++-5 g++-6 build-essential clang-3.7 clang-3.8 clang-3.9 llvm-${LLVM_VERSION}-dev python-pip
 		
 	
-	source bash /opt/qt${QT_VERSION:0:2}/bin/qt${QT_VERSION:0:2}-env.sh
+	source /opt/qt${QT_VERSION:0:2}/bin/qt${QT_VERSION:0:2}-env.sh
 	
 	./scripts/build_frameworks.sh -DCMAKE_BUILD_TYPE=$BUILD_TYPE -GNinja \
 		-DCMAKE_CXX_COMPILER=$CXX_COMPILER -DCMAKE_C_COMPILER=$C_COMPILER
