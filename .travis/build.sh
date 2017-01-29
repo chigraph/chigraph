067e8cad8999074a70141b1f546233082e5098dc
Russell Greene <russellgreene8@gmail.com>
2017-01-29 07:47:11 -0700

    Add mac to travis
    Required a split up of travis scripts, much better now :)

---
+++ .travis/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+if [ "$TRAVIS_OS_NAME" == "linux"]; then
+
+	covflags=""
+	if [ -n "$TEST_COV" ]; then 
+
+		covflags="-DCMAKE_CXX_FLAGS='-fprofile-arcs -ftest-coverage'"
+
+	fi 
+
+	cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE \ 
+		-DCMAKE_CXX_COMPILER=$CXX_COMPILER \ 
+		-DCMAKE_C_COMPILER=$C_COMPILER \ 
+		-DLLVM_CONFIG=/usr/lib/llvm-${LLVM_VERSION}/bin/llvm-config \ 
+		-DCLANG_EXE=`which clang-${LLVM_VERSION}` \ 
+		-DLLI_EXE=`which lli-${LLVM_VERSION}` \ 
+		-GNinja $covflags \ 
+
+	ninja
+	CTEST_OUTPUT_ON_FAILURE=1 ninja test
+
+	if [ -n "$TEST_COV" ]; then 
+		pip install --user cpp-coveralls 
+		coveralls --exclude .. --include ../libchig --include ../chig --exclude ../libchig/include/json.hpp
+	fi
+
+else 
+	
+	cmake .. \
+		-DCMAKE_PREFIX_PATH='/usr/local/opt/qt5/;/usr/local/opt/gettext' \
+		-DCMAKE_BUILD_TYPE=Debug \
+		-DLLVM_CONFIG=/usr/local/opt/llvm/bin/llvm-config \
+		-DLLI_EXE=/usr/local/opt/llvm/bin/lli -GNinja
+	ninja
+	# ninja test
+	
+fi
