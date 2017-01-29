9992dc7b52065cfde593b37b6d91c781e8dc7d5b
Russell Greene <russellgreene8@gmail.com>
2017-01-29 08:15:56 -0700

    Remove bad spaces

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,8 +6,8 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 
 	sudo apt-get update
 	sudo apt-get install qt${QT_VERSION:0:2}base qt${QT_VERSION:0:2}script \
-		qt${QT_VERSION:0:2}declarative qt${QT_VERSION:0:2}tools \ 
-		qt${QT_VERSION:0:2}x11extras qt${QT_VERSION:0:2}svg  ninja-build \ 
+		qt${QT_VERSION:0:2}declarative qt${QT_VERSION:0:2}tools \
+		qt${QT_VERSION:0:2}x11extras qt${QT_VERSION:0:2}svg  ninja-build \
 		libedit-dev libxcb-keysyms1-dev libxml2-utils gcc-5 gcc-6 g++-5 g++-6 \
 		build-essential clang-3.7 clang-3.8 clang-3.9 llvm-${LLVM_VERSION}-dev python-pip
 	source /opt/qt${QT_VERSION:0:2}/bin/qt${QT_VERSION:0:2}-env.sh
@@ -20,7 +20,7 @@ else
 
 	brew install cmake llvm wget qt5 bison gettext ninja
 	./scripts/build_frameworks.sh \
-		-DCMAKE_BUILD_TYPE=$BUILD_TYPE \ 
+		-DCMAKE_BUILD_TYPE=$BUILD_TYPE \
 		-DCMAKE_PREFIX_PATH='/usr/local/opt/qt5;/usr/local/opt/gettext' \
 		-DBISON_EXECUTABLE=/usr/local/opt/bison/bin/bison -GNinja
 	
