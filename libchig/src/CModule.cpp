dd228bce5052bd590f5f6f7f657bc82c6010df2c
Russell Greene <russellgreene8@gmail.com>
2017-02-13 21:47:56 -0700

    Refactor CModule to not compile on load but compile on codegen

--- libchig/src/CModule.cpp
+++ libchig/src/CModule.cpp
@@ -2,11 +2,13 @@
 
 #include "chig/CModule.hpp"
 #include "chig/Config.hpp"
+#include "chig/GraphFunction.hpp"
+#include "chig/JsonDeserializer.hpp"
 
 #include <libexecstream/exec-stream.h>
 
-#include <llvm/IRReader/IRReader.h>
 #include <llvm/IR/IRBuilder.h>
+#include <llvm/IRReader/IRReader.h>
 #include <llvm/Linker/Linker.h>
 #include <llvm/Support/SourceMgr.h>
 #include <llvm/Support/raw_ostream.h>
@@ -14,101 +16,108 @@
 
 using namespace chig;
 
+namespace {
+
+std::unique_ptr<llvm::Module> compileCCode(const std::string&              code,
+                                           const std::vector<std::string>& arguments,
+                                           llvm::LLVMContext& ctx, Result& res) {
+	std::string bitcode;
+	std::string error;
+	int         ec;
+	try {
+		// compile the C code
+		exec_stream_t clangexe;
+		clangexe.set_wait_timeout(exec_stream_t::s_out, 100000);
+
+		// get arguments to pass to clang
+		std::vector<std::string> args = {"-xc", "-", "-c", "-emit-llvm", "-g", "-O0", "-o", "-"};
+		args.reserve(args.size() + arguments.size());
+		std::copy(arguments.begin(), arguments.end(), std::back_inserter(args));
+
+		// start the executable
+		clangexe.start(std::string(CHIG_CLANG_EXE), args.begin(), args.end());
+
+		clangexe.in() << code;
+		clangexe.close_in();
+
+		bitcode = std::string(std::istreambuf_iterator<char>(clangexe.out()),
+		                      std::istreambuf_iterator<char>());
+		error = std::string(std::istreambuf_iterator<char>(clangexe.err()),
+		                    std::istreambuf_iterator<char>());
+
+		clangexe.close();
+		ec = clangexe.exit_code();
+
+	} catch (std::exception& e) {
+		res.addEntry("EUKN", "Failed to run clang and generate bitcode", {{"Error", e.what()}});
+		return nullptr;
+	}
+
+	if (ec != 0) {
+		res.addEntry("EUKN", "Error encountered while generating IR", {{"Error Text", error}});
+		return nullptr;
+	}
+
+	if (bitcode.empty()) {
+		res.addEntry("EUKN", "Unknown error encountered while generating IR", {});
+		return nullptr;
+	}
+
+	llvm::SMDiagnostic diag;
+	auto ret = llvm::parseIR(llvm::MemoryBufferRef(bitcode, "clang-generated"), diag, ctx);
+
+	if (!ret) {
+		std::string              errorString;
+		llvm::raw_string_ostream errorStream(errorString);
+		diag.print("chig compile", errorStream);
+
+		res.addEntry("EUKN", "Error parsing clang-generated bitcode module",
+		             {{"Error Text", errorString}});
+		return nullptr;
+	}
+
+	return ret;
+}
+
+}  // anon namespace
+
 /// The NodeType for calling C functions
 struct CFuncNode : NodeType {
-	CFuncNode(ChigModule& mod, gsl::cstring_span<> cCode, gsl::cstring_span<> functionName,
-	          Result& res)
+	CFuncNode(CModule& mod, gsl::cstring_span<> cCode, gsl::cstring_span<> functionName,
+	          std::vector<std::string> extraArgs, std::vector<NamedDataType> inputs,
+	          DataType output)
 	    : NodeType{mod, "func", "call C code"},
-	      functocall{gsl::to_string(functionName)},
-	      ccode(gsl::to_string(cCode)) {
+	      mFunctionName{gsl::to_string(functionName)},
+	      mCCode(gsl::to_string(cCode)),
+	      mExtraArguments{std::move(extraArgs)},
+	      mInputs{std::move(inputs)},
+	      mOutput{output},
+	      mCModule{mod} {
 		setExecInputs({""});
 		setExecOutputs({""});
 
-		std::string bitcode;
-		std::string error;
-		int ec;
-		try {
-			// compile the C code
-			exec_stream_t clangexe;
-			clangexe.set_wait_timeout(exec_stream_t::s_out, 100000);
-			std::vector<std::string> args = { "-xc", "-", "-S", "-emit-llvm", "-g", "-O0", "-I", "C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.10240.0\\ucrt", "-o", "-" };
-			clangexe.start(std::string(CHIG_CLANG_EXE), args.begin(), args.end());
-			
-			clangexe.in() << ccode;
-			clangexe.close_in();
-
-			bitcode = std::string(std::istreambuf_iterator<char>(clangexe.out()), std::istreambuf_iterator<char>());
-			error = std::string(std::istreambuf_iterator<char>(clangexe.err()), std::istreambuf_iterator<char>());
-
-			clangexe.close();
-			ec = clangexe.exit_code();
-
-		} catch (std::exception& e) {
-			res.addEntry("EUKN", "Failed to run clang and generate bitcode", {{"Error", e.what()}});
-			return;
-		}
-
-		if (ec != 0) {
-			res.addEntry("EUKN", "Error encountered while generating IR", {{"Error Text", error}});
-			return;
-		}
-
-		if (bitcode.empty()) {
-			res.addEntry("EUKN", "Unknown error encountered while generating IR", {});
-			return;
-		}
-
-		llvm::SMDiagnostic diag;
-		llcompiledmod = llvm::parseIR(llvm::MemoryBufferRef(bitcode, "clang-generated"), diag,
-		                                         context().llvmContext());
-
-		if (!llcompiledmod) {
-			std::string              errorString;
-			llvm::raw_string_ostream errorStream(errorString);
-			diag.print("chig compile", errorStream);
-
-			res.addEntry(
-			    "EUKN", "Error parsing clang-generated bitcode module",
-			    {{"Error Text", errorString}});
-			return;
-		}
-
-		auto llfunc = llcompiledmod->getFunction(functocall);
-
-		if (llfunc == nullptr) {
-			res.addEntry("EUKN", "Failed to get function in clang-compiled module",
-			             {{"Requested Function", functocall}});
-			return;
-		}
-
-		// get arguments
-		std::vector<NamedDataType> dInputs;
-		for (const auto& argument : llfunc->args()) {
-			DataType ty;
-			context().typeFromModule("lang", stringifyLLVMType(argument.getType()), &ty);
-			dInputs.emplace_back(
-			    argument.getName(),
-				ty);
-		}
-		setDataInputs(std::move(dInputs));
-
-		// get return type
-		auto ret = llfunc->getReturnType();
-
-		if (!ret->isVoidTy()) {
-			DataType ty;
-			context().typeFromModule("lang", stringifyLLVMType(ret), &ty);
-			setDataOutputs(
-			    {{"", ty}});
-		}
+		setDataInputs(mInputs);
+		if (mOutput.valid()) { setDataOutputs({{"", mOutput}}); }
 	}
 
 	Result codegen(size_t /*inID*/, const llvm::DebugLoc& nodeLocation,
-	               const gsl::span<llvm::Value*> io,
-	               llvm::BasicBlock*                  codegenInto,
-	               const gsl::span<llvm::BasicBlock*> outputBlocks, std::unordered_map<std::string, std::shared_ptr<void>>& compileCache) const override {
-		Expects(io.size() == dataInputs().size() + dataOutputs().size() &&
-		        codegenInto != nullptr && outputBlocks.size() == 1);
+	               const gsl::span<llvm::Value*> io, llvm::BasicBlock* codegenInto,
+	               const gsl::span<llvm::BasicBlock*> outputBlocks,
+	               std::unordered_map<std::string, std::shared_ptr<void>>& compileCache) override {
+		Expects(io.size() == dataInputs().size() + dataOutputs().size() && codegenInto != nullptr &&
+		        outputBlocks.size() == 1);
+
+		Result res;
+
+		// compile the c code if it hasn't already been compiled
+
+		if (llcompiledmod == nullptr) {
+			std::vector<std::string> args = mCModule.extraArguments();
+			std::copy(mExtraArguments.begin(), mExtraArguments.end(), std::back_inserter(args));
+			llcompiledmod = compileCCode(mCCode, args, context().llvmContext(), res);
+
+			if (!res) { return res; }
+		}
 
 		// create a copy of the module
 		auto copymod = llvm::CloneModule(llcompiledmod.get());
@@ -123,7 +132,7 @@ struct CFuncNode : NodeType {
 
 		codegenInto->getModule()->setDataLayout("");
 
-		auto llfunc = codegenInto->getModule()->getFunction(functocall);
+		auto llfunc = codegenInto->getModule()->getFunction(mFunctionName);
 		Expects(llfunc != nullptr);
 
 		llvm::IRBuilder<> builder(codegenInto);
@@ -151,15 +160,30 @@ struct CFuncNode : NodeType {
 		auto brInst = builder.CreateBr(outputBlocks[0]);
 		brInst->setDebugLoc(nodeLocation);
 
-		return {};
+		return res;
 	}
 
 	nlohmann::json toJSON() const override {
 		auto j = nlohmann::json{};
 
 		j             = nlohmann::json::object();
-		j["code"]     = ccode;
-		j["function"] = functocall;
+		j["code"]     = mCCode;
+		j["function"] = mFunctionName;
+
+		auto& jsonExtraFlags = j["extraflags"];
+		for (const auto& flag : mExtraArguments) { jsonExtraFlags.push_back(flag); }
+
+		auto& jsonInputs = j["inputs"];
+		jsonInputs       = nlohmann::json::array();
+		for (const auto& in : mInputs) {
+			jsonInputs.push_back({{in.name, in.type.qualifiedName()}});
+		}
+
+		if (mOutput.valid()) {
+			j["output"] = mOutput.qualifiedName();
+		} else {
+			j["output"] = nlohmann::json();
+		}
 
 		return j;
 	}
@@ -167,18 +191,23 @@ struct CFuncNode : NodeType {
 	std::unique_ptr<NodeType> clone() const override {
 		Result res;
 
-		return std::make_unique<CFuncNode>(module(), ccode, functocall, res);
+		return std::make_unique<CFuncNode>(mCModule, mCCode, mFunctionName, mExtraArguments,
+		                                   mInputs, mOutput);
 	}
 
-	std::string functocall;
-	std::string ccode;
+	std::string                mFunctionName;
+	std::string                mCCode;
+	std::vector<std::string>   mExtraArguments;
+	std::vector<NamedDataType> mInputs;
+	DataType                   mOutput;
+	CModule&                   mCModule;
 
 	std::unique_ptr<llvm::Module> llcompiledmod;
 };
 
 CModule::CModule(Context& ctx) : ChigModule(ctx, "c") {}
 
-DataType                  CModule::typeFromName(gsl::cstring_span<> /*typeName*/) {
+DataType CModule::typeFromName(gsl::cstring_span<> /*typeName*/) {
 	// TODO: implement
 
 	return {};
@@ -217,11 +246,106 @@ Result CModule::nodeTypeFromName(gsl::cstring_span<> typeName, const nlohmann::j
 			    {{"Given Data"}, jsonData});
 		}
 
-		*toFill = std::make_unique<CFuncNode>(*this, code, function, res);
+		std::vector<std::string> extraFlags;
+		if (jsonData.is_object() && jsonData.find("extraflags") != jsonData.end() &&
+		    jsonData["extraflags"].is_array()) {
+			for (const auto& flag : jsonData["extraflags"]) {
+				std::string str = flag;
+				extraFlags.emplace_back(std::move(str));
+			}
+		} else {
+			res.addEntry("WUKN", "Data for c:func must have an extraflags array",
+			             {{"Given Data"}, jsonData});
+		}
+
+		std::vector<NamedDataType> inputs;
+		if (jsonData.is_object() && jsonData.find("inputs") != jsonData.end() &&
+		    jsonData["inputs"].is_array()) {
+			inputs.reserve(jsonData["inputs"].size());
+			for (const auto& jsonArg : jsonData["inputs"]) {
+				std::string docstring, qualifiedName;
+				std::tie(docstring, qualifiedName) = parseObjectPair(jsonArg);
+
+				std::string moduleName, typeName;
+				std::tie(moduleName, typeName) = parseColonPair(qualifiedName);
+
+				DataType ty;
+				res += context().typeFromModule(moduleName, typeName, &ty);
+
+				inputs.emplace_back(docstring, ty);
+			}
+
+		} else {
+			res.addEntry("WUKN", "Data for c:func must have an inputs array",
+			             {{"Given Data"}, jsonData});
+		}
+
+		DataType output;
+		if (jsonData.is_object() && jsonData.find("output") != jsonData.end()) {
+			auto& outputJson = jsonData["output"];
+
+			// if it's a string, there's a return type
+			if (outputJson.is_string()) {
+				std::string moduleName, typeName;
+				std::tie(moduleName, typeName) = parseColonPair(outputJson);
+
+				res += context().typeFromModule(moduleName, typeName, &output);
+			}
+			// now if it isn't null, then there was an error
+			else if (!outputJson.is_null()) {
+				res.addEntry("WUKN",
+				             R"("output" element in c:func must be either null or a string)",
+				             {{"Given Data"}, jsonData});
+			}
+		} else {
+			res.addEntry(
+			    "WUKN",
+			    "Data for c:func must have an output element that is either null or a string",
+			    {{"Given Data"}, jsonData});
+		}
+
+		*toFill = std::make_unique<CFuncNode>(*this, code, function, extraFlags, inputs, output);
 		return res;
 	}
 
 	res.addEntry("E37", "Unrecognized node type in module",
 	             {{"Module", "c"}, {"Requested Type", name()}});
+	return res;
+}
+
+Result chig::CModule::createNodeTypeFromCCode(const std::string&              code,
+                                              const std::string&              functionName,
+                                              const std::vector<std::string>& clangArgs,
+                                              std::unique_ptr<NodeType>*      toFill) {
+	Expects(toFill != nullptr);
+
+	Result res;
+
+	auto mod = compileCCode(code, clangArgs, context().llvmContext(), res);
+
+	if (!res) { return res; }
+
+	auto llFunc = mod->getFunction(functionName);
+
+	if (llFunc == nullptr) {
+		res.addEntry("EUKN", "Failed to find function in C code",
+		             {{"Function Name", functionName}, {"C Code", code}});
+		return res;
+	}
+
+	std::vector<NamedDataType> dInputs;
+	for (const auto& argument : llFunc->args()) {
+		DataType ty;
+		context().typeFromModule("lang", stringifyLLVMType(argument.getType()), &ty);
+		dInputs.emplace_back(argument.getName(), ty);
+	}
+
+	DataType output;
+	auto     ret = llFunc->getReturnType();
+
+	if (!ret->isVoidTy()) { context().typeFromModule("lang", stringifyLLVMType(ret), &output); }
+
+	*toFill = std::make_unique<CFuncNode>(*this, code, functionName, clangArgs, dInputs, output);
+
 	return res;
 }
