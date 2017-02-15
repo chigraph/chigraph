630f129765055a4bb9fce15fd7e6bbe3e060546e
Russell Greene <russellgreene8@gmail.com>
2017-02-15 07:27:56 -0700

    Fix script

--- scripts/download_llvm.ps1
+++ scripts/download_llvm.ps1
@@ -1,14 +1,15 @@
 $scriptsdir = $PSScriptRoot
 
-if ($args.length < 1) {
+if ($args.length -ne 1) {
 	"Usage: download_llvm.ps1 [Debug|Release]"
+	exit
 }
 
 
 $type = $args[0]
 
 $url = ""
-if ($type.CompareTo("Debug")) {
+if ($type -eq "Debug") {
 	$url = "https://github.com/chigraph/chigraph/releases/download/dependencies/llvm-3.9.1-debug-msvc14-win64.zip"
 } else {
 	$url = "https://github.com/chigraph/chigraph/releases/download/dependencies/llvm-3.9.1-release-msvc14-win64.zip"
@@ -19,7 +20,8 @@ cd $scriptsdir\..\third_party
 # download the zip file
 $webclient = New-Object System.Net.WebClient
 
-$webclient.DownloadFile("$url", "$pwd/$llvm-$type.zip")
+"downloading llvm-$type.zip"
+$webclient.DownloadFile("$url", "$pwd/llvm-$type.zip")
 
 
-Expand-Archive "$llvm-$type.zip" .
+Expand-Archive "llvm-$type.zip" .
