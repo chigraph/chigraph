864219131572b208d1629f10fd55d4c5473ccca0
Russell Greene <russellgreene8@gmail.com>
2017-02-23 00:01:15 -0700

    Work more on build scripts

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,10 +3,7 @@
 set -xe
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-	
-	./scripts/build_frameworks.sh Debug "-DCMAKE_BUILD_TYPE=$BUILD_TYPE -GNinja -DCMAKE_CXX_COMPILER=$CXX_COMPILER -DCMAKE_C_COMPILER=$C_COMPILER"
-	
-
+	./setup.py
 else
 
 	brew install cmake qt5 bison gettext ninja || echo
