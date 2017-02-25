c4e4263f969c21752acd401b237513009655f26d
Russell Greene <russellgreene8@gmail.com>
2017-02-25 10:37:17 -0700

    Try to emulate pressing X

--- scripts/setup_msys2.sh
+++ scripts/setup_msys2.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-#pacman -Syu --noconfirm
-
-pacman -Sy --noconfirm
+(
+	pacman -Syu --noconfirm && ( kill $$ )
+)
+	
 pacman -S --noconfirm mingw64-x86_64-toolchain mingw64-x86_64-llvm mingw64-x86_64-clang mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja mingw-w64-x86_64-qt5 git bison flex mingw-w64-x86_64-python3 
 
