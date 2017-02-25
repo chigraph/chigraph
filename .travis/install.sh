49e5e64dc147ea57d19b0de2e7f62d52d6c9a830
Russell Greene <russellgreene8@gmail.com>
2017-02-25 08:03:48 -0700

    Update install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,8 +8,7 @@ else
 
 	brew install cmake qt5 bison gettext ninja || echo
 	brew install llvm --with-clang
-	./scripts/build_frameworks.sh Debug "-DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_PREFIX_PATH='/usr/local/opt/qt5;/usr/local/opt/gettext' -DBISON_EXECUTABLE=/usr/local/opt/bison/bin/bison -GNinja"
-	
+		
 fi
 	
 
