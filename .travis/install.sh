7eb1c9cc788bd0d0b6095156ba91d5074882323f
Russell Greene <russellgreene8@gmail.com>
2017-02-25 08:15:42 -0700

    Wokrk on more sutff

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,10 +4,12 @@ set -xe
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 	./setup.py
+	find . -maxdepth 2
 else
 
 	brew install cmake qt5 bison gettext ninja || echo
 	brew install llvm --with-clang
+	/setup.py
 		
 fi
 	
