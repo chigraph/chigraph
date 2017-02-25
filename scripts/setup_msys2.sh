5b25fc57d212ba7517184c0e0e01909f833d1b1a
Russell Greene <russellgreene8@gmail.com>
2017-02-25 10:58:38 -0700

    Work on msys

--- scripts/setup_msys2.sh
+++ scripts/setup_msys2.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-(
-	pacman -Syu --noconfirm && ( kill $$ )
-)
-	
+
+pacman -Syu --noconfirm
+
 pacman -S --noconfirm mingw64-x86_64-toolchain mingw64-x86_64-llvm mingw64-x86_64-clang mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja mingw-w64-x86_64-qt5 git bison flex mingw-w64-x86_64-python3 
 
