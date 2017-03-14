5b10c2d3a61e51866848e8a5c2b88e5c101b838f
Russell Greene <russellgreene8@gmail.com>
2017-03-14 17:03:37 -0600

    Work more on GUI debugger

--- gui/functionview.hpp
+++ gui/functionview.hpp
@@ -45,6 +45,8 @@ private slots:
 	void nodeMoved(QtNodes::Node& n, QPointF newLoc);
 
 	void nodeDoubleClicked(QtNodes::Node& n);
+	
+	void addBreakpoint(QtNodes::Node& n);
 
 signals:
 	void dirtied();
