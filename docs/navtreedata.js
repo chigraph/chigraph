df5d898daf922e602db94595c457a21d532fd09f
Russell Greene <russellgreene8@gmail.com>
2017-03-19 16:07:12 -0600

    Update docs

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -1,34 +1,13 @@
 var NAVTREE =
 [
   [ "chigraph", "index.html", [
-    [ "Chigraph API Documentation", "index.html", null ],
-    [ "Namespaces", null, [
-      [ "Namespace List", "namespaces.html", "namespaces" ],
-      [ "Namespace Members", "namespacemembers.html", [
-        [ "All", "namespacemembers.html", null ],
-        [ "Functions", "namespacemembers_func.html", null ]
-      ] ]
-    ] ],
-    [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", "functions_dup" ],
-        [ "Functions", "functions_func.html", null ],
-        [ "Variables", "functions_vars.html", null ]
-      ] ]
-    ] ],
-    [ "Files", null, [
-      [ "File List", "files.html", "files" ]
-    ] ]
+    [ "Chigraph API Documentation", "index.html", null ]
   ] ]
 ];
 
 var NAVTREEINDEX =
 [
-".html",
-"structchig_1_1DataType.html#aa63396ca8c48bd0a6be166df245772a9"
+"index.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,33 +1,18 @@
 var indexSectionsWithContent =
 {
-  0: "acdefghijlmnopqrstuvwxy~",
-  1: "cdghlnr",
-  2: "c",
-  3: "cdfglnr",
-  4: "acdefghijlmnopqrstuvwxy~",
-  5: "inorst",
-  6: "c"
+  0: "c",
+  1: "c"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "variables",
-  6: "pages"
+  1: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes",
-  2: "Namespaces",
-  3: "Files",
-  4: "Functions",
-  5: "Variables",
-  6: "Pages"
+  1: "Pages"
 };
 
