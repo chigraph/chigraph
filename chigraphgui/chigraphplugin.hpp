2bc9c4c9590999f4a754094f7ae559306578073d
Russell Greene <russellgreene8@gmail.com>
2017-03-20 17:50:46 -0600

    Get the plugin system working!

---
+++ chigraphgui/chigraphplugin.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifndef CHIGRAPHGUI_CHIGRAPH_PLUGIN
+#define CHIGRAPHGUI_CHIGRAPH_PLUGIN
+
+#include "toolview.hpp"
+
+#include <QtPlugin>
+#include <QVector>
+
+
+class ChigraphPlugin : public KXMLGUIClient {
+public:
+	virtual ~ChigraphPlugin() = default;
+	
+	virtual QVector<ToolView*> toolViews() = 0;
+};
+
+// tell qt that it's a thing
+#define ChigraphPlugin_iid "org.chigraph.chigraphgui.ChigraphPlugin"
+Q_DECLARE_INTERFACE(ChigraphPlugin, ChigraphPlugin_iid)
+
+
+#endif // CHIGRAPHGUI_CHIGRAPH_PLUGIN
