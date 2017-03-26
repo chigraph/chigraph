30152cfee96d0c391ead1796303871fc64463685
Russell Greene <russellgreene8@gmail.com>
2017-03-26 09:34:20 -0600

    Update screenshot, bug in subprocessoutputview, appimage scripts

---
+++ scripts/upload_chigraph_dockerimage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPTSDIR/appimage
+
+docker build . -t russelltg/chigraph-appimage:latest
+docker push russelltg/chigraph-appimage:latest
