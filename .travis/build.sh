caa73eeb64095da6b23424a576b2ba2aaba23b80
Russell Greene <russellgreene8@gmail.com>
2017-04-02 10:18:34 -0600

    Remove LLI_EXE from osx travis build, fix appimage_recipie to not be so verbose

--- .travis/build.sh
+++ .travis/build.sh
@@ -35,7 +35,7 @@ else
 		-DCMAKE_PREFIX_PATH='/usr/local/opt/qt5/;/usr/local/opt/gettext' \
 		-DCMAKE_BUILD_TYPE=Debug \
 		-DLLVM_CONFIG=/usr/local/opt/llvm/bin/llvm-config \
-		-DLLI_EXE=/usr/local/opt/llvm/bin/lli -GNinja -DCG_BUILD_DEBUGGER=OFF
+		-GNinja -DCG_BUILD_DEBUGGER=OFF
 	ninja
 	# ninja test
 	
--- scripts/appimage/appimage_recipie.sh
+++ scripts/appimage/appimage_recipie.sh
@@ -31,7 +31,7 @@ ln -s lib lib64
 
 # build KF5
 /chigraph/setup.sh
-rsync -raP /chigraph/third_party/kf5-release/* /chigraph.appdir/usr/
+rsync -raPq /chigraph/third_party/kf5-release/* /chigraph.appdir/usr/
 
 # build chigraph
 cd /chigraph
