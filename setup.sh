cfe410508c449dbbc82f110a1eedaab2bd13e38a
Russell Greene <russellgreene8@gmail.com>
2017-04-03 20:46:10 -0600

    Use KF5 5.32.0

--- setup.sh
+++ setup.sh
@@ -19,8 +19,8 @@ tpdir="$CHIGRAPHDIR/third_party"
 
 if [  "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 	
-	dlAndExtract 'https://github.com/chigraph/chigraph/releases/download/dependencies/kf5-debug-gcc-linux64.tar.xz' "$tpdir/kf5-debug.tar.xz" $tpdir
-	dlAndExtract 'https://github.com/chigraph/chigraph/releases/download/dependencies/kf5-release-gcc-linux64.tar.xz' "$tpdir/kf5-release.tar.xz" $tpdir
+	dlAndExtract 'https://github.com/chigraph/chigraph/releases/download/dependencies/kf5-5.32.0-debug-gcc-linux64.tar.xz' "$tpdir/kf5-debug.tar.xz" $tpdir
+	dlAndExtract 'https://github.com/chigraph/chigraph/releases/download/dependencies/kf5-5.32.0-release-gcc-linux64.tar.xz' "$tpdir/kf5-release.tar.xz" $tpdir
 	
 	
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ] || [ "$(expr substr $(uname -s) 1 7)" == "MSYS_NT" ]; then
