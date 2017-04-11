7ffdb6624ccb18bfb09904652ed6d33c54512ff4
Russell Greene <russellgreene8@gmail.com>
2017-04-10 20:49:17 -0600

    Work on CI

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,33 +3,20 @@
 set -xe
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-
-	if [ -n "$TEST_COV" ]; then 
-
-		cmake . -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-			-DCMAKE_CXX_COMPILER=$CXX_COMPILER \
-			-DCMAKE_C_COMPILER=$C_COMPILER \
-			-GNinja -DCMAKE_CXX_FLAGS='--coverage' \
-			-DLLVM_CONFIG="/usr/lib/llvm-${LLVM_VERSION}/bin/llvm-config"
-	else
-
-		cmake . -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-			-DCMAKE_CXX_COMPILER=$CXX_COMPILER \
-			-DCMAKE_C_COMPILER=$C_COMPILER \
-			-GNinja \
-			-DLLVM_CONFIG="/usr/lib/llvm-${LLVM_VERSION}/bin/llvm-config"
-			
-	fi
-
+	
+	cmake . -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+		-DCMAKE_CXX_COMPILER=$CXX_COMPILER \
+		-DCMAKE_C_COMPILER=$C_COMPILER \
+		-GNinja -DCMAKE_CXX_FLAGS='--coverage' \
+		-DLLVM_CONFIG="/usr/lib/llvm-${LLVM_VERSION}/bin/llvm-config"
+	
 	ninja
 	CTEST_OUTPUT_ON_FAILURE=1 ninja test
 	
-	if [ -n "$TEST_COV" ]; then 
-		pip install --user cpp-coveralls 
-		coveralls --exclude /usr --exclude third_party --exclude chigraphgui --exclude test --exclude build --include libchigraph --include libchigraphdebugger --include chi --exclude libchigraph/include/chi/json.hpp
-	fi
-
-else 
+	pip install --user cpp-coveralls 
+	coveralls --exclude /usr --exclude third_party --exclude test --exclude build --include libchigraph --include libchigraphdebugger --include chi --exclude libchigraph/include/chi/json.hpp
+	
+else
 	
 	cmake . \
 		-DCMAKE_PREFIX_PATH='/usr/local/opt/qt5/;/usr/local/opt/gettext' \
