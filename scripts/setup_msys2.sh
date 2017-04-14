6b0be09f6d43832a205bf2655d42bc783e0164ed
Russell Greene <russellgreene8@gmail.com>
2017-04-14 09:46:59 -0600

    Remove setup.sh call from msys CI

--- scripts/setup_msys2.sh
+++ scripts/setup_msys2.sh
@@ -7,6 +7,4 @@ cd $SCRIPTSDIR/..
 
 pacman -Syu --noconfirm
 
-pacman -S --noconfirm mingw-w64-x86_64-toolchain mingw-w64-x86_64-llvm mingw-w64-x86_64-clang mingw-w64-x86_64-clang-tools-extra mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja mingw-w64-x86_64-qt5 git bison flex mingw-w64-x86_64-python3 
-
-./setup.sh
+pacman -S --noconfirm mingw-w64-x86_64-toolchain mingw-w64-x86_64-llvm mingw-w64-x86_64-clang mingw-w64-x86_64-clang-tools-extra mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja git 
