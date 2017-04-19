b43606818ed19dc1d1b8a199dd9d154ea7e104c5
Russell Greene <russellgreene8@gmail.com>
2017-04-18 18:22:14 -0600

    Work on ctollvm for llvm 3.7-

--- libchigraph/src/ctollvm.cpp
+++ libchigraph/src/ctollvm.cpp
@@ -19,6 +19,82 @@ using namespace llvm;
 using namespace clang;
 using namespace std;
 
+// clang 3.7- don't do this right at all.
+#if LLVM_VERSION_MAJOR <= 3 && LLVM_VERSION_MINOR <= 7
+std::unique_ptr<CompilerInvocation> createInvocationFromCommandLineBackport (
+    ArrayRef<const char *> ArgList,
+    IntrusiveRefCntPtr<DiagnosticsEngine> Diags) {
+  if (!Diags.get()) {
+    // No diagnostics engine was provided, so create our own diagnostics object
+    // with the default options.
+    Diags = CompilerInstance::createDiagnostics(new DiagnosticOptions);
+  }
+
+  SmallVector<const char *, 16> Args(ArgList.begin(), ArgList.end());
+
+  // FIXME: Find a cleaner way to force the driver into restricted modes.
+  Args.push_back("-fsyntax-only");
+
+  // FIXME: We shouldn't have to pass in the path info.
+  driver::Driver TheDriver(Args[0], llvm::sys::getDefaultTargetTriple(),
+                           *Diags);
+
+  // Don't check that inputs exist, they may have been remapped.
+  TheDriver.setCheckInputsExist(false);
+
+  std::unique_ptr<driver::Compilation> C(TheDriver.BuildCompilation(Args));
+
+  // Just print the cc1 options if -### was present.
+  if (C->getArgs().hasArg(driver::options::OPT__HASH_HASH_HASH)) {
+    C->getJobs().Print(llvm::errs(), "\n", true);
+    return nullptr;
+  }
+
+  // We expect to get back exactly one command job, if we didn't something
+  // failed. Offload compilation is an exception as it creates multiple jobs. If
+  // that's the case, we proceed with the first job. If caller needs a
+  // particular job, it should be controlled via options (e.g.
+  // --cuda-{host|device}-only for CUDA) passed to the driver.
+  const driver::JobList &Jobs = C->getJobs();
+  bool OffloadCompilation = false;
+  if (Jobs.size() > 1) {
+    for (auto &A : C->getActions()){
+      // On MacOSX real actions may end up being wrapped in BindArchAction
+      if (isa<driver::BindArchAction>(A))
+        A = *A->input_begin();
+      if (isa<driver::OffloadAction>(A)) {
+        OffloadCompilation = true;
+        break;
+      }
+    }
+  }
+  if (Jobs.size() == 0 || !isa<driver::Command>(*Jobs.begin()) ||
+      (Jobs.size() > 1 && !OffloadCompilation)) {
+    SmallString<256> Msg;
+    llvm::raw_svector_ostream OS(Msg);
+    Jobs.Print(OS, "; ", true);
+    Diags->Report(diag::err_fe_expected_compiler_job) << OS.str();
+    return nullptr;
+  }
+
+  const driver::Command &Cmd = cast<driver::Command>(*Jobs.begin());
+  if (StringRef(Cmd.getCreator().getName()) != "clang") {
+    Diags->Report(diag::err_fe_expected_clang_command);
+    return nullptr;
+  }
+
+  const ArgStringList &CCArgs = Cmd.getArguments();
+  auto CI = llvm::make_unique<CompilerInvocation>();
+  if (!CompilerInvocation::CreateFromArgs(*CI,
+                                     const_cast<const char **>(CCArgs.data()),
+                                     const_cast<const char **>(CCArgs.data()) +
+                                     CCArgs.size(),
+                                     *Diags))
+    return nullptr;
+  return CI;
+}
+#endif
+
 std::unique_ptr<llvm::Module> cToLLVM(LLVMContext& ctx, const char* execPath, const char* code,
                                       const char* fileName, std::vector<const char*> compileArgs,
                                       std::string& err) {
@@ -46,7 +122,14 @@ std::unique_ptr<llvm::Module> cToLLVM(LLVMContext& ctx, const char* execPath, co
 #if LLVM_VERSION_MAJOR >= 4
 	    std::shared_ptr<clang::CompilerInvocation>
 #endif
-	    (clang::createInvocationFromCommandLine(compileArgs, diagnosticsEngine));
+	    (
+#if LLVM_VERSION_MAJOR <= 3 && LLVM_VERSION_MINOR <= 7
+			createInvocationFromCommandLineBackport
+#else
+			clang::createInvocationFromCommandLine
+#endif
+			(compileArgs, diagnosticsEngine)
+		);
 
 	Clang->setInvocation(invoc);
 
