5ed3f1cf91e0aec1691baffd5a38c7569d931ed8
Russell Greene <russellgreene8@gmail.com>
2017-04-30 17:23:04 -0600

    Fix pretty major bug in LLVM_VERSION_AT_LEAST

--- libchigraph/include/chi/LLVMVersion.hpp
+++ libchigraph/include/chi/LLVMVersion.hpp
@@ -6,6 +6,6 @@
 #include <llvm/Config/llvm-config.h>
 
 #define LLVM_VERSION_LESS_EQUAL(major, minor) ((LLVM_VERSION_MAJOR == major && LLVM_VERSION_MINOR <= minor) || (LLVM_VERSION_MAJOR < major))
-#define LLVM_VERSION_AT_LEAST(major, minor) ((LLVM_VERSION_MAJOR == major && LLVM_VERSION >= minor) || (LLVM_VERSION_MAJOR > major))
+#define LLVM_VERSION_AT_LEAST(major, minor) ((LLVM_VERSION_MAJOR == major && LLVM_VERSION_MINOR >= minor) || (LLVM_VERSION_MAJOR > major))
 
 #endif // CHI_LLVM_VERSION_HPP
