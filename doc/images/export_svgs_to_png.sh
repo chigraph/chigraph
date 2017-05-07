559d830efcc6813efe7ee4485d50318a3b100b95
Russell Greene <russellgreene8@gmail.com>
2017-05-07 11:52:08 -0600

    Move icon to gui

--- doc/images/export_svgs_to_png.sh
+++ doc/images/export_svgs_to_png.sh
@@ -7,8 +7,5 @@ imgdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ink=`which inkscape`
 $ink -e chigraph.png -d 150 chigraph.svg
-$ink -e chigraphsmall.png -d 150 chigraphsmall.svg
-
-
 
 
