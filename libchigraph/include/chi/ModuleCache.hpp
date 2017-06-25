f5efe143c2ad74fdbc15991766ad668c178c370c
Russell Greene <russellgreene8@gmail.com>
2017-06-25 16:01:32 -0600

    Make ModuleCache a virtual interface for modularity

--- libchigraph/include/chi/ModuleCache.hpp
+++ libchigraph/include/chi/ModuleCache.hpp
@@ -12,10 +12,13 @@
 
 namespace chi {
 
-/// This class caches modules into the lib/ direcotry of the workspace
+/// This class  provides an interface for creating module caches
 struct ModuleCache {
 	/// Create a module cache with a bound context
-	explicit ModuleCache(Context& ctx);
+	explicit ModuleCache(Context& ctx) : mContext{&ctx} {}
+	
+	/// Destructor
+	virtual ~ModuleCache() = default;
 
 	// No copy or move
 	ModuleCache(const ModuleCache&) = delete;
@@ -30,31 +33,26 @@ struct ModuleCache {
 	/// \param timeAtFileRead The time to store as the cache time. Should be the time the module was
 	/// read from disk
 	/// \return The Result
-	Result cacheModule(const boost::filesystem::path& moduleName, llvm::Module& compiledModule,
-	                   std::time_t timeAtFileRead);
+	virtual Result cacheModule(const boost::filesystem::path& moduleName, llvm::Module& compiledModule,
+	                   std::time_t timeAtFileRead) = 0;
 
-	/// Get the cache name for a module. Basically `context().workspacePath() / moduleName + ".bc"`
-	/// \param moduleName The name of the module to get a cache path for
-	/// \return The path
-	boost::filesystem::path cachePathForModule(const boost::filesystem::path& moduleName) const;
-
-	/// Inavlidate the cache, as it delete the cache file
+	/// Inavlidate the cache, ie. delete the cache file
 	/// \param moduleName The name of the module to invalidate
 	/// \pre `!moduleName.empty()`
-	void invalidateCache(const boost::filesystem::path& moduleName);
+	virtual void invalidateCache(const boost::filesystem::path& moduleName) = 0;
 
 	/// Get the time that a cache was updated
 	/// \param moduleName The module to get the last update time for
 	/// \return The time it was updated, or 0 if there is no cache
-	std::time_t cacheUpdateTime(const boost::filesystem::path& moduleName) const;
+	virtual std::time_t cacheUpdateTime(const boost::filesystem::path& moduleName) const = 0;
 
 	/// Retrieve a module from the cache
 	/// \param moduleName The name of the module to retrieve
 	/// \pre `!moduleName.empty()`
 	/// \param atLeastThisNew Make sure the cache is at least as new as this
 	/// \return A llvm::Module, or nullptr if no suitable cache was found
-	std::unique_ptr<llvm::Module> retrieveFromCache(const boost::filesystem::path& moduleName,
-	                                                std::time_t                    atLeastThisNew);
+	virtual std::unique_ptr<llvm::Module> retrieveFromCache(const boost::filesystem::path& moduleName,
+	                                                std::time_t                    atLeastThisNew) = 0;
 
 	/// Get the context this cache is bound to
 	/// \return the `Context`
