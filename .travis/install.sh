b86b090ddeb545a8626d56510fc90dc53d3e5362
Russell Greene <russellgreene8@gmail.com>
2017-06-28 21:10:10 -0600

    Work on non-sudo script

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,17 +3,7 @@
 set -xe
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-	
-	sudo apt-get update && sudo apt-get install $PACKAGES \
-		llvm-${LLVM_VERSION}-dev libclang-${LLVM_VERSION}-dev \
-		libclang-common-${LLVM_VERSION}-dev ninja-build \
-		libedit-dev build-essential
-	
-	if [ "$LLVM_VERSION" == "3.9" ] || [ "$LLVM_VERSION" == "4.0" ]; then
-		sudo apt-get install liblldb-${LLVM_VERSION}-dev
-	else
-		sudo apt-get install lldb-${LLVM_VERSION}-dev
-	fi
+	;
 else
 
 	brew install cmake ninja || echo
