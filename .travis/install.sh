ccf70487a194fb8f97b26ffd887715443bc5b269
Russell Greene <russellgreene8@gmail.com>
2017-06-28 21:02:58 -0600

    Re-add sudo?

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,15 +4,15 @@ set -xe
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 	
-	apt-get update && apt-get install $PACKAGES \
+	sudo apt-get update && sudo apt-get install $PACKAGES \
 		llvm-${LLVM_VERSION}-dev libclang-${LLVM_VERSION}-dev \
 		libclang-common-${LLVM_VERSION}-dev ninja-build \
 		libedit-dev build-essential
 	
 	if [ "$LLVM_VERSION" == "3.9" ] || [ "$LLVM_VERSION" == "4.0" ]; then
-		apt-get install liblldb-${LLVM_VERSION}-dev
+		sudo apt-get install liblldb-${LLVM_VERSION}-dev
 	else
-		apt-get install lldb-${LLVM_VERSION}-dev
+		sudo apt-get install lldb-${LLVM_VERSION}-dev
 	fi
 else
 
