3eaa42fce4db3ac80555ac958deb447c5e0c291d
Russell Greene <russellgreene8@gmail.com>
2017-11-04 22:14:40 -0600

    Fix test because repo moved

--- test/FetchTests.cpp
+++ test/FetchTests.cpp
@@ -21,8 +21,8 @@ TEST_CASE("Contexts can fetch remote modules", "[Context]") {
 	// create the .chigraphworkspace file
 	{ fs::ofstream stream{workspaceDir / ".chigraphworkspace"}; }
 
-	auto res = fetchModule(workspaceDir, "github.com/russelltg/hellochigraph/hello/main", true);
+	auto res = fetchModule(workspaceDir, "github.com/chigraph/hellochigraph/hello/main", true);
 	REQUIRE(res.dump() == "");
 
-	REQUIRE(fs::is_directory(workspaceDir / "src" / "github.com" / "russelltg" / "hellochigraph"));
+	REQUIRE(fs::is_directory(workspaceDir / "src" / "github.com" / "chigraph" / "hellochigraph"));
 }
