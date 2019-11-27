83fd07af6762829a349c91acc33bf27fb0395fc4
Russell Greene <russellgreene8@gmail.com>
2019-11-27 13:31:11 -0500

    handle uuid parsing errors, fixes unit test

--- lib/support/include/chi/Support/Uuid.hpp
+++ lib/support/include/chi/Support/Uuid.hpp
@@ -12,32 +12,40 @@ namespace chi {
 
 class Uuid {
 public:
-	Uuid() = default;
+	Uuid() noexcept = default;
 	template <typename InputIterator>
-	Uuid(InputIterator begin, InputIterator end) {
+	Uuid(InputIterator begin, InputIterator end) noexcept {
 		std::copy(begin, end, mData.begin());
 	}
-	~Uuid() = default;
+	~Uuid() noexcept = default;
 
-	static Uuid                random();
-	static std::optional<Uuid> fromString(std::string_view from);
+	static Uuid                random() noexcept;
+	static std::optional<Uuid> fromString(std::string_view from) noexcept;
 
-	const std::array<uint8_t, 16>& data() const { return mData; }
+	const std::array<uint8_t, 16>& data() const noexcept { return mData; }
 
-	std::string toString() const;
-	size_t      hash() const;
-	bool        valid() const;
+	std::string toString() const noexcept;
+	size_t      hash() const noexcept;
+	bool        valid() const noexcept;
 
 private:
 	std::array<uint8_t, 16> mData = {};
 };
 
-inline bool operator==(const Uuid& lhs, const Uuid& rhs) { return lhs.data() == rhs.data(); }
-inline bool operator!=(const Uuid& lhs, const Uuid& rhs) { return lhs.data() != rhs.data(); }
-inline bool operator<(const Uuid& lhs, const Uuid& rhs) { return lhs.data() < rhs.data(); }
-inline bool operator<=(const Uuid& lhs, const Uuid& rhs) { return lhs.data() <= rhs.data(); }
-inline bool operator>(const Uuid& lhs, const Uuid& rhs) { return lhs.data() > rhs.data(); }
-inline bool operator>=(const Uuid& lhs, const Uuid& rhs) { return lhs.data() >= rhs.data(); }
+inline bool operator==(const Uuid& lhs, const Uuid& rhs) noexcept {
+	return lhs.data() == rhs.data();
+}
+inline bool operator!=(const Uuid& lhs, const Uuid& rhs) noexcept {
+	return lhs.data() != rhs.data();
+}
+inline bool operator<(const Uuid& lhs, const Uuid& rhs) noexcept { return lhs.data() < rhs.data(); }
+inline bool operator<=(const Uuid& lhs, const Uuid& rhs) noexcept {
+	return lhs.data() <= rhs.data();
+}
+inline bool operator>(const Uuid& lhs, const Uuid& rhs) noexcept { return lhs.data() > rhs.data(); }
+inline bool operator>=(const Uuid& lhs, const Uuid& rhs) noexcept {
+	return lhs.data() >= rhs.data();
+}
 
 }  // namespace chi
 
@@ -45,6 +53,6 @@ inline bool operator>=(const Uuid& lhs, const Uuid& rhs) { return lhs.data() >= 
 namespace std {
 template <>
 struct hash<chi::Uuid> {
-	size_t operator()(const chi::Uuid& uuid) const { return uuid.hash(); }
+	size_t operator()(const chi::Uuid& uuid) const noexcept { return uuid.hash(); }
 };
 }  // namespace std
--- lib/support/src/Uuid.cpp
+++ lib/support/src/Uuid.cpp
@@ -6,30 +6,34 @@
 #include <boost/uuid/string_generator.hpp>
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_io.hpp>
-
 #include <numeric>
 
 namespace chi {
 
-Uuid Uuid::random() {
+Uuid Uuid::random() noexcept {
 	auto uuid = boost::uuids::random_generator()();
 	Uuid ret;
 	std::copy(uuid.data, uuid.data + sizeof(uuid.data), ret.mData.begin());
 	return ret;
 }
-std::optional<Uuid> Uuid::fromString(std::string_view from) {
-	auto uuid = boost::uuids::string_generator()(from.data());
-	return Uuid{uuid.data, uuid.data + sizeof(uuid.data)};
+std::optional<Uuid> Uuid::fromString(std::string_view from) noexcept {
+	try {
+		auto uuid = boost::uuids::string_generator()(from.data());
+		return Uuid{uuid.data, uuid.data + sizeof(uuid.data)};
+	} catch (const std::runtime_error&) {
+		// failed to parse, return none
+		return {};
+	}
 }
-std::string Uuid::toString() const {
+std::string Uuid::toString() const noexcept {
 	boost::uuids::uuid uuid;
 	std::copy(mData.begin(), mData.end(), uuid.data);
 	return boost::uuids::to_string(uuid);
 }
-size_t Uuid::hash() const {
+size_t Uuid::hash() const noexcept {
 	boost::uuids::uuid uuid;
 	std::copy(mData.begin(), mData.end(), uuid.data);
 	return boost::uuids::hash_value(uuid);
 }
-bool Uuid::valid() const { return std::accumulate(mData.begin(), mData.end(), 0) != 0; }
+bool Uuid::valid() const noexcept { return std::accumulate(mData.begin(), mData.end(), 0) != 0; }
 }  // namespace chi
